35568c75f6f05c1f0ad5f90f3a8f56acee9da52e
Karl Stoney <me@karlstoney.com>
2017-02-26 11:50:02 +0000

    JSHint styling of template

--- template.js
+++ template.js
@@ -12,15 +12,15 @@ exports.description = 'Create a node.js module, including mocha for tests, and b
 
 // Template-specific notes to be displayed before question prompts.
 exports.notes = '_Project name_ shouldn\'t contain "node" or "js" and should ' +
-	'be a unique ID not already in use at search.npmjs.org.';
+  'be a unique ID not already in use at search.npmjs.org.';
 
 // Template-specific notes to be displayed after question prompts.
 exports.after = 'You should now install project dependencies with _npm ' +
-	'install_. After that, you may execute project tasks with _grunt_. For ' +
-	'more information about installing and configuring Grunt, please see ' +
-	'the Getting Started guide:' +
-	'\n\n' +
-	'http://gruntjs.com/getting-started';
+  'install_. After that, you may execute project tasks with _grunt_. For ' +
+  'more information about installing and configuring Grunt, please see ' +
+  'the Getting Started guide:' +
+  '\n\n' +
+  'http://gruntjs.com/getting-started';
 
 // Any existing file or directory matching this wildcard will cause a warning.
 exports.warnOn = '*';
@@ -28,72 +28,73 @@ exports.warnOn = '*';
 // The actual init template.
 exports.template = function(grunt, init, done) {
 
-	init.process({
-		type: 'node'
-	}, [
-		// Prompt for these values.
-		init.prompt('name'),
-		init.prompt('description'),
-		init.prompt('version'),
-		init.prompt('repository'),
-		init.prompt('homepage'),
-		init.prompt('bugs'),
-		init.prompt('licenses'),
-		init.prompt('author_name'),
-		init.prompt('author_email'),
-		init.prompt('author_url'),
-		init.prompt('node_version', '>= 0.8.0'),
-		init.prompt('main'),
-		init.prompt('npm_test', 'grunt mochaTest'), {
-			name: 'coveralls',
-			message: 'Will you use coveralls to report the coverage status?',
-			default: 'Y/n',
-			warning: 'If selected, you must enable Coveralls in https://coveralls.io'
-		}, {
-			name: 'travis',
-			message: 'Will this project be tested with Travis CI?',
-			default: 'Y/n',
-			warning: 'If selected, you must enable Travis support for this project in https://travis-ci.org/profile'
-		},
-	], function(err, props) {
-		props.keywords = [];
-		props.devDependencies = {
-			'grunt': '^1.0.1',
-			'grunt-contrib-jshint': '^1.1.0',
-			'grunt-contrib-watch': '^1.0.0',
-			'grunt-mocha-istanbul': '^5.0.2',
-			'grunt-mocha-test': '^0.13.2',
-			'grunt-notify': '0.4.5',
-			'istanbul': '^0.4.5',
-			'mocha': '^3.2.0',
-			'mocha-jenkins-reporter': '^0.3.7',
-			'should': '^11.2.0'
-		};
-		props.travis = /y/i.test(props.travis);
-		props.coveralls = /y/i.test(props.coveralls);
+  init.process({
+    type: 'node'
+  }, [
+    // Prompt for these values.
+    init.prompt('name'),
+    init.prompt('description'),
+    init.prompt('version'),
+    init.prompt('repository'),
+    init.prompt('homepage'),
+    init.prompt('bugs'),
+    init.prompt('licenses'),
+    init.prompt('author_name'),
+    init.prompt('author_email'),
+    init.prompt('author_url'),
+    init.prompt('node_version', '>= 7.0.0'),
+    init.prompt('main'),
+    init.prompt('npm_test', 'grunt mochaTest'),
+    {
+      name: 'coveralls',
+      message: 'Will you use coveralls to report the coverage status?',
+      default: 'y/N',
+      warning: 'If selected, you must enable Coveralls in https://coveralls.io'
+    }, {
+      name: 'travis',
+      message: 'Will this project be tested with Travis CI?',
+      default: 'y/N',
+      warning: 'If selected, you must enable Travis support for this project in https://travis-ci.org/profile'
+    },
+  ], function(err, props) {
+    props.keywords = [];
+    props.devDependencies = {
+      'grunt': '^1.0.1',
+      'grunt-contrib-jshint': '^1.1.0',
+      'grunt-contrib-watch': '^1.0.0',
+      'grunt-mocha-istanbul': '^5.0.2',
+      'grunt-mocha-test': '^0.13.2',
+      'grunt-notify': '0.4.5',
+      'istanbul': '^0.4.5',
+      'mocha': '^3.2.0',
+      'mocha-jenkins-reporter': '^0.3.7',
+      'should': '^11.2.0'
+    };
 
-		// Add coveralls dependencies if required
-		if (props.coveralls) {
-			props.devDependencies.coveralls = '^2.11.16';
-		}
+    props.travis = !(/n/i.test(props.travis));
+    props.coveralls = !(/n/i.test(props.coveralls));
+    // Add coveralls dependencies if required
+    if (props.coveralls) {
+      props.devDependencies.coveralls = '^2.11.16';
+    }
 
-		// Files to copy (and process).
-		var files = init.filesToCopy(props);
-		if (!props.travis) {
-			delete files['.travis.yml'];
-		}
+    // Files to copy (and process).
+    var files = init.filesToCopy(props);
+    if (!props.travis) {
+      delete files['.travis.yml'];
+    }
 
-		// Add properly-named license files.
-		init.addLicenseFiles(files, props.licenses);
+    // Add properly-named license files.
+    init.addLicenseFiles(files, props.licenses);
 
-		// Actually copy (and process) files.
-		init.copyAndProcess(files, props);
+    // Actually copy (and process) files.
+    init.copyAndProcess(files, props);
 
-		// Generate package.json file.
-		init.writePackageJSON('package.json', props);
+    // Generate package.json file.
+    init.writePackageJSON('package.json', props);
 
-		// All done!
-		done();
-	});
+    // All done!
+    done();
+  });
 
 };
